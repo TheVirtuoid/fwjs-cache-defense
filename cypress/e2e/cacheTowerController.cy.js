e88ea66296618c33d1516253f92a67bea90b1157
TheVirtuoid <thevirtuoid@gmail.com>
2023-09-19 20:03:03 -0400

    errors in controller code

--- cypress/e2e/cacheTowerController.cy.js
+++ cypress/e2e/cacheTowerController.cy.js
@@ -44,6 +44,28 @@ describe('cache controller', () => {
 			const cacheTowerController = new CacheTowerController();
 			expect(() => cacheTowerController.adjustCacheTowerHealth(1)).to.throw(CacheTowerController.ERROR_ADJUSTCACHETOWERHEALTH_NO_CACHETOWER.message);
 		});
+		describe('when cacheTower is present', () => {
+			let cacheTowerController;
+			beforeEach(() => {
+				cacheTowerController = new CacheTowerController();
+				cacheTowerController.newCacheTower();
+			});
+			it('should throw exception if argument is not an integer', () => {
+				expect(() => cacheTowerController.adjustCacheTowerHealth('bad')).to.throw(CacheTower.ERROR_ADJUSTHEALTH_ARGUMENT_NOT_INTEGER.message);
+			});
+			it('should add to the health', () => {
+				cacheTowerController.adjustCacheTowerHealth(1);
+				expect(cacheTowerController.health).to.equal(CacheTowerController.DEFAULT_HEALTH + 1);
+			});
+			it('should subtract from the health', () => {
+				cacheTowerController.adjustCacheTowerHealth(-1);
+				expect(cacheTowerController.health).to.equal(CacheTowerController.DEFAULT_HEALTH - 1);
+			});
+			it('should set to 0 if adjustment went below 0', () => {
+				cacheTowerController.adjustCacheTowerHealth(-CacheTowerController.DEFAULT_HEALTH * 2);
+				expect(cacheTowerController.health).to.equal(0);
+			});
+		});
 	});
 
 	describe('know when game is over', () => {
