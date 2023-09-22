9c7074c57fa8ae0d7574b32d1a3b9d2bbd16bb5a
TheVirtuoid <thevirtuoid@gmail.com>
2023-09-22 10:00:54 -0400

    Working on Monsters running!

--- cypress/e2e/baseClasses/monster.cy.js
+++ cypress/e2e/baseClasses/monster.cy.js
@@ -19,5 +19,113 @@ describe('monster', () => {
 			const monster = new Monster({ type: MonsterType.ALIEN, id: 'test' });
 			expect(monster.id).to.equal('test');
 		});
+		it('should set a sub-position to default', () => {
+			const monster = new Monster({ type: MonsterType.ALIEN });
+			const subPosition = monster.getSubPosition();
+			expect(subPosition.x).to.equal(Number.POSITIVE_INFINITY);
+			expect(subPosition.y).to.equal(Number.POSITIVE_INFINITY);
+		});
+		it('should set speed to default', () => {
+			const monster = new Monster({ type: MonsterType.ALIEN });
+			const { x, y } = monster.getSpeed();
+			expect(x).to.equal(Monster.DEFAULT_SPEED.x);
+			expect(y).to.equal(Monster.DEFAULT_SPEED.y);
+		});
+	});
+
+	describe('getSubPosition', () => {
+		it('should return the sub-position', () => {
+			const monster = new Monster({ type: MonsterType.ALIEN });
+			const subPosition = monster.getSubPosition();
+			expect(subPosition.x).to.equal(Number.POSITIVE_INFINITY);
+			expect(subPosition.y).to.equal(Number.POSITIVE_INFINITY);
+		});
+	});
+
+	describe('setSubPosition', () => {
+		it('should throw error if x is not a number', () => {
+			const monster = new Monster({ type: MonsterType.ALIEN });
+			expect(() => monster.setSubPosition('bad', .2)).to.throw(Monster.ERROR_SUBPOSITION_X_NOT_NUMBER.message);
+		});
+		it('should throw error if y is not a number', () => {
+			const monster = new Monster({ type: MonsterType.ALIEN });
+			expect(() => monster.setSubPosition(.1, 'bad')).to.throw(Monster.ERROR_SUBPOSITION_Y_NOT_NUMBER.message);
+		});
+		it('should throw error if x is less than 0', () => {
+			const monster = new Monster({ type: MonsterType.ALIEN });
+			expect(() => monster.setSubPosition(-1, .2)).to.throw(Monster.ERROR_SUBPOSITION_X_OUT_OF_RANGE.message);
+		});
+		it('should throw error if y is less than 0', () => {
+			const monster = new Monster({ type: MonsterType.ALIEN });
+			expect(() => monster.setSubPosition(.1, -1)).to.throw(Monster.ERROR_SUBPOSITION_Y_OUT_OF_RANGE.message);
+		});
+		it('should throw error if x is 1 or greater', () => {
+			const monster = new Monster({ type: MonsterType.ALIEN });
+			expect(() => monster.setSubPosition(1, .1)).to.throw(Monster.ERROR_SUBPOSITION_X_OUT_OF_RANGE.message);
+		});
+		it('should throw error if y is 1 or greater', () => {
+			const monster = new Monster({ type: MonsterType.ALIEN });
+			expect(() => monster.setSubPosition(.1, 1)).to.throw(Monster.ERROR_SUBPOSITION_Y_OUT_OF_RANGE.message);
+		});
+		it('should set the sub-position', () => {
+			const monster = new Monster({ type: MonsterType.ALIEN });
+			monster.setSubPosition(.1, .2);
+			const subPosition = monster.getSubPosition();
+			expect(subPosition.x).to.equal(.1);
+			expect(subPosition.y).to.equal(.2);
+		});
+	});
+
+	describe('hasSubPositionBeenSet', () => {
+		it('should return false is nothing has been set', () => {
+			const monster = new Monster({ type: MonsterType.ALIEN });
+			expect(monster.hasSubPositionBeenSet()).to.be.false;
+		});
+		it('should return true if something has been set', () => {
+			const monster = new Monster({ type: MonsterType.ALIEN });
+			monster.setSubPosition(.1, .2);
+			expect(monster.hasSubPositionBeenSet()).to.be.true;
+		});
+	});
+
+	describe('getSpeed', () => {
+		it('should get the correct speed', () => {
+			const monster = new Monster({ type: MonsterType.ALIEN });
+			monster.setSpeed(.1, .2);
+			const { x, y } = monster.getSpeed();
+			expect(x).to.equal(.1);
+			expect(y).to.equal(.2);
+		});
+	})
+
+	describe('setSpeed', () => {
+		let monster;
+		beforeEach(() => {
+			monster = new Monster({ type: MonsterType.ALIEN });
+		});
+		it(`should throw error if "x" argument is not a number`, () => {
+			expect(() => monster.setSpeed('bad', .2)).to.throw(Monster.ERROR_SETSPEED_X_NOT_NUMBER.message);
+		});
+		it(`should throw error if "x" argument is -1 or less`, () => {
+			expect(() => monster.setSpeed(-1, .2)).to.throw(Monster.ERROR_SETSPEED_X_OUT_OF_RANGE.message);
+		});
+		it(`should throw error if "x" argument is 1 or greater`, () => {
+			expect(() => monster.setSpeed(1, .2)).to.throw(Monster.ERROR_SETSPEED_X_OUT_OF_RANGE.message);
+		});
+		it(`should throw error if "y" argument is not a number`, () => {
+			expect(() => monster.setSpeed(.1, 'bad')).to.throw(Monster.ERROR_SETSPEED_Y_NOT_NUMBER.message);
+		});
+		it(`should throw error if "y" argument is -1 or less`, () => {
+			expect(() => monster.setSpeed(.1, -1)).to.throw(Monster.ERROR_SETSPEED_Y_OUT_OF_RANGE.message);
+		});
+		it(`should throw error if "y" argument is 1 or greater`, () => {
+			expect(() => monster.setSpeed(.1, 1)).to.throw(Monster.ERROR_SETSPEED_Y_OUT_OF_RANGE.message);
+		});
+		it('should set the speed', () => {
+			monster.setSpeed(.1, .2);
+			const { x, y } = monster.getSpeed();
+			expect(x).to.equal(.1);
+			expect(y).to.equal(.2);
+		});
 	});
 });
--- src/classes/Monster.js
+++ src/classes/Monster.js
@@ -3,9 +3,27 @@ import MonsterType from "./types/MonsterType.js";
 
 export default class Monster extends BaseGameItem {
 
+	static DEFAULT_SPEED = { x: 0, y: 0 };
+
 	static ERROR_INVALID_MONSTER_TYPE = new TypeError(`"type" property not a valid Monster Type`);
+	static ERROR_SUBPOSITION_X_NOT_NUMBER = new TypeError(`"x" property is not a number`);
+	static ERROR_SUBPOSITION_Y_NOT_NUMBER = new TypeError(`"y" property is not a number`);
+	static ERROR_SUBPOSITION_X_OUT_OF_RANGE = new RangeError(`"x" property must be between 0 and .999, inclusive.`);
+	static ERROR_SUBPOSITION_Y_OUT_OF_RANGE = new RangeError(`"y" property must be between 0 and .999, inclusive.`);
+	static ERROR_SETSPEED_X_NOT_NUMBER = new TypeError(`"x" property is not a number`);
+	static ERROR_SETSPEED_Y_NOT_NUMBER = new TypeError(`"y" property is not a number`);
+	static ERROR_SETSPEED_X_OUT_OF_RANGE = new RangeError(`"x" property must be between -1 and 1, exclusive.`);
+	static ERROR_SETSPEED_Y_OUT_OF_RANGE = new RangeError(`"y" property must be between -1 and 1, exclusive.`);
 
 	#type;
+	#subPosition = {
+		x: Number.POSITIVE_INFINITY,
+		y: Number.POSITIVE_INFINITY
+	};
+	#speed = {
+		x: Monster.DEFAULT_SPEED.x,
+		y: Monster.DEFAULT_SPEED.y
+	};
 
 	constructor(args = {}) {
 		const { type } = args;
@@ -14,5 +32,62 @@ export default class Monster extends BaseGameItem {
 		}
 		super(args);
 		this.#type = type;
+		this.#changeSubPosition(Number.POSITIVE_INFINITY, Number.POSITIVE_INFINITY);
+	}
+
+	getSubPosition() {
+		return {
+			x: this.#subPosition.x,
+			y: this.#subPosition.y
+		}
+	}
+
+	hasSubPositionBeenSet() {
+		return !(this.#subPosition.x === Number.POSITIVE_INFINITY || this.#subPosition.y === Number.POSITIVE_INFINITY);
+	}
+
+	setSubPosition(x, y) {
+		if (typeof(x) !== 'number') {
+			throw Monster.ERROR_SUBPOSITION_X_NOT_NUMBER;
+		}
+		if (typeof(y) !== 'number') {
+			throw Monster.ERROR_SUBPOSITION_Y_NOT_NUMBER;
+		}
+		if (x < 0 || x >= 1) {
+			throw Monster.ERROR_SUBPOSITION_X_OUT_OF_RANGE;
+		}
+		if (y < 0 || y >= 1) {
+			throw Monster.ERROR_SUBPOSITION_Y_OUT_OF_RANGE;
+		}
+		this.#changeSubPosition(x, y);
+	}
+
+	getSpeed() {
+		return {
+			x: this.#speed.x,
+			y: this.#speed.y
+		}
+	}
+
+	setSpeed(x, y) {
+		if (typeof(x) !== 'number') {
+			throw Monster.ERROR_SETSPEED_X_NOT_NUMBER;
+		}
+		if (typeof(y) !== 'number') {
+			throw Monster.ERROR_SETSPEED_Y_NOT_NUMBER;
+		}
+		if (x <= -1 || x >=1) {
+			throw Monster.ERROR_SETSPEED_X_OUT_OF_RANGE;
+		}
+		if (y <= -1 || y >=1) {
+			throw Monster.ERROR_SETSPEED_Y_OUT_OF_RANGE;
+		}
+		this.#speed.x = x;
+		this.#speed.y = y;
+	}
+
+	#changeSubPosition(x, y) {
+		this.#subPosition.x = x;
+		this.#subPosition.y = y;
 	}
 }
